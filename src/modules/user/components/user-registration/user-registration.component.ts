6ca9f0ce2f59266c93aa5c86ef73db3bfbbb3a8d
Lexa <sanon@et.intechinfo.fr>
2021-01-28 09:52:18 +0100

    sign up Lv II

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import {UserQueries} from '../../services/user.queries';
+import {NzMessageService} from 'ng-zorro-antd/message';
 
 class UserRegistrationFormModel {
   username = "";
@@ -19,27 +21,42 @@ export class UserRegistrationComponent implements OnInit {
   form: NgForm;
 
   model = new UserRegistrationFormModel();
+  requiredField = 'Ce champ est obligatoire';
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private userQueries: UserQueries,
+    private nzMessageService: NzMessageService
   ) { }
 
   ngOnInit(): void {
   }
 
+  async usernameTest(){
+    if (await this.userQueries.exists(this.model.username)) {
+      this.nzMessageService.error('Ce username exsiste déjà');
+    }
+  }
+
   async submit() {
 
     // TODO  Vérifier que la confirmation de mot de passe correspond au mot de passe
-    if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
+    if (this.form.form.invalid ) {
+      this.nzMessageService.error('Une erreur est survenue. Veuillez réessayer plus tard');
       return;
     }
-
+    if ( this.model.password !== this.model.confirmPassword){
+      this.nzMessageService.error('Les mots de passe ne corespondent pas');
+      return;
+    }
     // TODO Enregistrer l'utilisateur via le UserService
-    this.goToLogin();
+    await this.userService.register(this.model.username, this.model.password);
+    await this.goToLogin();
   }
 
-  goToLogin() {
+  async goToLogin() {
     // TODO rediriger l'utilisateur sur "/splash/login"
+    await this.router.navigate(['/splash/login']);
   }
 }
